adfa704a109b66c45a5c4f95ca34f76e3d4bcc6e
yeeeeonwoo <ways007@naver.com>
2025-02-06 16:44:42 +0900

    Create 알람 시계_2884.c

---
+++ 2단계/알람시계_2884.c
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void) {
+	int h, m;
+	scanf("%d %d", &h, &m);
+	if (m >= 45) {
+		printf("%d %d", h, m - 45);
+	}
+	else {
+		if (h == 0) {
+			printf("23 %d", 60 - (45 - m));
+		}
+		else {
+			printf("%d %d", h - 1, 60 - (45 - m));
+		}
+	}
+}
