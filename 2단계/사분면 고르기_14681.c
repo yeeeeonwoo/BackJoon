7ffe0131bd154da4b7328928823562e6c502732e
yeeeeonwoo <ways007@naver.com>
2025-02-06 16:38:05 +0900

    Create 사분면 고르기_14681.c

---
+++ 2단계/사분면 고르기_14681.c
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void) {
+	int x, y;
+	scanf("%d %d", &x, &y);
+	if (x > 0) {
+		if (y > 0) {
+			printf("1");
+		}
+		else {
+			printf("4");
+		}
+	}
+	else {
+		if (y > 0) {
+			printf("2");
+		}
+		else {
+			printf("3");
+		}
+	}
+}
