66ff5c061fd13c9671a660fb3dac95110e9257ac
yeeeeonwoo <ways007@naver.com>
2025-02-09 16:26:36 +0900

    Create 새싹_25083.c

---
+++ 6단계/새싹_25083.c
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+
+int main(void) {
+	printf("         ,r\'\"7\n");
+	printf("r\`-_   ,\'  ,/\n");
+	printf(" \\. \". L_r\'\n");
+	printf("   \`~\\/\n");
+	printf("      |\n");
+	printf("      |");
+}
