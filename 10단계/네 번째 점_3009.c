c748b00c48b7e41de9129ab63611492726276ec6
yeeeeonwoo <ways007@naver.com>
2025-02-11 16:22:36 +0900

    Create 네 번째 점_3009.c

---
+++ 10단계/네 번째 점_3009.c
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+	int x1, y1, x2, y2, x3, y3;
+	scanf("%d %d", &x1, &y1);
+	scanf("%d %d", &x2, &y2);
+	scanf("%d %d", &x3, &y3);
+
+	if (x1 == x2) printf("%d ", x3);
+	else if (x2 == x3) printf("%d ", x1);
+	else if (x1 == x3) printf("%d ", x2);
+
+	if (y1 == y2) printf("%d", y3);
+	else if (y2 == y3) printf("%d", y1);
+	else if (y1 == y3) printf("%d", y2);
+}
