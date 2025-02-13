d8098f98598e9042836a257a3a766b165c513954
yeeeeonwoo <ways007@naver.com>
2025-02-13 14:41:34 +0900

    Create 알고리즘 수업 - 알고리즘의 수행 시간 3_24264.c

---
+++ 11단계/알고리즘 수업 - 알고리즘의 수행 시간 3_24264.c
@@ -0,0 +1,9 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+    long long n; //n^2는 약 범위가 2500억이므로 int범위넘어감(int범위: -21억~21억)
+    scanf("%lld", &n);
+    printf("%lld\n", n*n); //시간복잡도는 n*n
+    printf("2"); // O(N*N)이므로 2
+}
